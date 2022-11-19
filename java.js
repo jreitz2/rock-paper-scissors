05d87aca61c983ba47654951520d1d2ca86a58f3
jreitz2 <jreitz2@gmail.com>
2022-11-19 15:32:25 -0500

    Add UI

--- java.js
+++ java.js
@@ -1,79 +1,115 @@
-function getComputerChoice() {
-    let computerSelection = ["Charmander", "Squirtle", "Bulbasaur"]
-    return computerSelection[Math.floor(Math.random() * computerSelection.length)]
-}
+// function getComputerChoice() {
+//     let computerSelection = ["Charmander", "Squirtle", "Bulbasaur"]
+//     return computerSelection[Math.floor(Math.random() * computerSelection.length)]
+// }
 
-let playerSelection = prompt("Choose your Pokemon!");
-console.log(playerSelection, "chosen by player.");
+// let computerSelection = getComputerChoice();
+// console.log(computerSelection, "chosen by computer.");
 
-let computerSelection = getComputerChoice();
-console.log(computerSelection, "chosen by computer.");
-
+let playerSelection = "";
 let playerScore = 0;
 let computerScore = 0;
 
 function playRound(computerSelection, playerSelection){
-    let result ="";
+    
+    const results = document.querySelector(".results");
 
     //computer is Charmander
     if (computerSelection == "Charmander" && playerSelection == "Squirtle") {
-        result = "Squirtle douses Charmander! You win!"
+        results.textContent = "Squirtle douses Charmander! You win!";
         playerScore++;
+        //console.log("Player score = ", playerScore);
     }
     else if (computerSelection == "Charmander" && playerSelection == "Bulbasaur") {
-        result = "Charmander burns Bulbasaur! You lose!"
+        results.textContent = "Charmander burns Bulbasaur! You lose!";
         computerScore++;
     }
     else if (computerSelection == "Charmander" && playerSelection == "Charmander") {
-        result = "Tie!"
-        
+        results.textContent = "Tie!";
     }
     // Computer is Squirtle
     else if (computerSelection == "Squirtle" && playerSelection == "Charmander") {
-        result = "Squirtle douses Charmander! You lose!"
+        results.textContent = "Squirtle douses Charmander! You lose!";
         computerScore++;
     }
     else if (computerSelection == "Squirtle" && playerSelection == "Bulbasaur") {
-        result = "Bulbasaur vine whips Squirtle! You win!"
+        results.textContent = "Bulbasaur vine whips Squirtle! You win!";
         playerScore++;
     }
     else if (computerSelection == "Squirtle" && playerSelection == "Squirtle") {
-        result = "Tie!"
+        results.textContent = "Tie!";
                 
     }
     // computer is Bulbasaur
     else if (computerSelection == "Bulbasaur" && playerSelection == "Squirtle") {
-        result = "Bulbasaur vine whips Squirtle! You lose!"
+        results.textContent = "Bulbasaur vine whips Squirtle! You lose!";
         computerScore++;
     }
     else if (computerSelection == "Bulbasaur" && playerSelection == "Charmander") {
-        result = "Charmander burns Bulbasaur! You win!"
+        results.textContent = "Charmander burns Bulbasaur! You win!";
         playerScore++;
     }
     else if (computerSelection == "Bulbasaur" && playerSelection == "Bulbasaur") {
-        result = "Tie!"
+        results.textContent = "Tie!";
         
     }
-    return result;
+    const compChoice = document.querySelector(".comp");
+    compChoice.innerHTML = `The computer chose: ${computerSelection}`;
+    const score = document.querySelector(".score");
+    score.innerHTML = `Player: ${playerScore} / Computer: ${computerScore}`;
+
+    const results2 = document.querySelector(".results2");
+        if (playerScore == 5) {
+            results2.textContent = `You won 5 rounds!`;
+            playerScore = 0;
+            computerScore = 0;
+            score.innerHTML = `Player: ${playerScore} / Computer: ${computerScore}`;
+        }
+        if (computerScore == 5) {
+            results2.textContent = `The computer won 5 rounds!`;
+            computerScore = 0;
+            playerScore = 0;
+            score.innerHTML = `Player: ${playerScore} / Computer: ${computerScore}`;
+        }
 }
 
-console.log(playRound(computerSelection, playerSelection));
-
-console.log("Player score = ", playerScore)
-console.log("Computer score = ", computerScore)
-
-// Loop the game 5 times
-
-/*
-function game() {
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt("Choose pokemon");
-        let computerSelection = getComputerChoice();
-        playRound(computerSelection, playerSelection);
+const charChosen = document.querySelector(".char");
+charChosen.addEventListener("click", () => {
+    playerSelection = "Charmander";
+    console.log(playerSelection, "chosen by player.");
+    function getComputerChoice() {
+        let computerSelection = ["Charmander", "Squirtle", "Bulbasaur"]
+        return computerSelection[Math.floor(Math.random() * computerSelection.length)]
     }
-}
-console.log(game());
+    let computerSelection = getComputerChoice();
+    console.log(computerSelection, "chosen by computer.");
+    playRound(computerSelection, playerSelection); 
+})
 
-console.log("Player score = ", playerScore)
-console.log("Computer score = ", computerScore)
-*/
+const squiChosen = document.querySelector(".squi");
+squiChosen.addEventListener("click", () => {
+    playerSelection = "Squirtle";
+    console.log(playerSelection, "chosen by player.");
+    function getComputerChoice() {
+        let computerSelection = ["Charmander", "Squirtle", "Bulbasaur"]
+        return computerSelection[Math.floor(Math.random() * computerSelection.length)]
+    }
+    let computerSelection = getComputerChoice();
+    console.log(computerSelection, "chosen by computer.");
+    playRound(computerSelection, playerSelection); 
+})
+
+const bulbChosen = document.querySelector(".bulb");
+bulbChosen.addEventListener("click", () => {
+    playerSelection = "Bulbasaur";
+    console.log(playerSelection, "chosen by player.");
+    function getComputerChoice() {
+        let computerSelection = ["Charmander", "Squirtle", "Bulbasaur"]
+        return computerSelection[Math.floor(Math.random() * computerSelection.length)]
+    }
+    let computerSelection = getComputerChoice();
+    console.log(computerSelection, "chosen by computer.");
+    playRound(computerSelection, playerSelection); 
+})
+
+
